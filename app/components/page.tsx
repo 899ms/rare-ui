8005283a4389dfdf1dcc2f218ef65686045c6f79
swamimalode07 <swami.malode22@vit.edu>
2026-09-18 16:49:53 +0530

    carbon

--- app/components/page.tsx
+++ app/components/page.tsx
@@ -67,7 +67,7 @@ export default async function ComponentsIndexPage() {
           </section>
         ))}
 
-        <CarbonAds className="mx-auto mt-14 w-full max-w-100" />
+        <CarbonAds count={2} className="mx-auto mt-14 w-full max-w-204" />
       </main>
 
       <Footer />
